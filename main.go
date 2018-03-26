0065d8df6a2e2fd8a64a908ee0019829deb946a2
jbs1987 <36437807+jbs1987@users.noreply.github.com>
2018-03-26 11:47:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -22,15 +22,15 @@ func main() {
 	for num <= 100 { //counter ends in 100
 		//if the number of countes is multiple of 3 print CD
 		if multiplo(num, 3) == true && multiplo(num, 5) == false {
-			fmt.Println("CD")
+			fmt.Println("\x1b[33;1mCD\x1b[0m")
 		}
 		//if the number of countes is multiple of 5 print CD
 		if multiplo(num, 3) == false && multiplo(num, 5) == true {
-			fmt.Println("mon")
+			fmt.Println("\x1b[34;1mmon\x1b[0m")
 		}
 		//if the number of countes is multiple of 3 and 5 at same time, print CDmon
 		if multiplo(num, 3) == true && multiplo(num, 5) == true {
-			fmt.Println("CDmon")
+			fmt.Println("\x1b[32;1mCDmon\x1b[0m")
 		}
 		//rest of cases
 		if multiplo(num, 3) == false && multiplo(num, 5) == false {
